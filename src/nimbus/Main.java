f0df9d1bcff4672a14778863464fd2b187360fb0
rooper <logan.rooper@gmail.com>
2015-11-07 17:29:04 -0500

    added interface

--- src/nimbus/Main.java
+++ src/nimbus/Main.java
@@ -3,10 +3,27 @@
  */
 package nimbus;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
 /**
  * @author loganrooper
  *
  */
-public class Main {
-
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage theStage) {
+		theStage.setTitle("Project Nimbus");
+	}
+	
+	
+	public void initializeInterface() {
+		
+	}
+	
+	
 }
---
+++ src/nimbus/wundergroundapi.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package nimbus;
+
+/**
+ * @author loganrooper
+ *
+ */
+public class wundergroundapi {
+	public wundergroundapi() {
+		String key = "ecbe43c694d84375";
+	}
+			
+	
+}
